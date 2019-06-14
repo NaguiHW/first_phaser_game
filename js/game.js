43f26c13755340cad0087a5b8a0901efae4d2970
Kalib Hackin <nagui08@hotmail.com>
2019-06-14 16:50:41 -0500

    Adding the background

--- js/game.js
+++ js/game.js
@@ -1,6 +1,17 @@
 // new scene
 let gameScene = new Phaser.Scene('Game');
 
+gameScene.preload = function(){
+    this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player.png');
+};
+
+gameScene.create = function(){
+    let bg = this.add.sprite(0, 0, 'background');
+
+    bg.setPosition(config.width/2, config.height/2);
+};
+
 // configuration of the game
 let config = {
     type: Phaser.AUTO,
