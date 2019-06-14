6eaf4d60775f6cbf953411b4283c1f269c735ad8
Kalib Hackin <nagui08@hotmail.com>
2019-06-14 17:39:35 -0500

    adding the player and the enemy

--- js/game.js
+++ js/game.js
@@ -1,15 +1,30 @@
-// new scene
+// create new scene
 let gameScene = new Phaser.Scene('Game');
 
+// load all images
 gameScene.preload = function(){
+    // loadgind background image
     this.load.image('background', 'assets/background.png');
+
+    // loading player image
     this.load.image('player', 'assets/player.png');
+    
+    // loading dragon image
+    this.load.image('enemy', 'assets/dragon.png');
 };
 
+// call the images
 gameScene.create = function(){
-    let bg = this.add.sprite(0, 0, 'background');
+    // calling the background
+    let bg = this.add.sprite(config.width/2, config.height/2, 'background');
 
-    bg.setPosition(config.width/2, config.height/2);
+    // calling the player
+    let player = this.add.sprite(50, config.height/2, 'player');
+    
+    // calling the player
+    let enemy = this.add.sprite(250, config.height/2, 'enemy');
+    enemy.flipX = true;
+    enemy.setScale(0.65, 0.65);
 };
 
 // configuration of the game
